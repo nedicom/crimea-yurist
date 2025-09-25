22cc482a46d4bd78fc3e06a5d22755fce60aeb76
nedicom <m6132@yandex.ru>
2025-09-25 19:28:51 +0300

    test

---
+++ static/js/metrika.js
@@ -0,0 +1,6 @@
+// Yandex Metrica с задержкой
+(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,
+k.src=r,a.parentNode.insertBefore(k,a)})
+(window,document,'script','https://mc.yandex.ru/metrika/tag.js?id=104280599','ym');
+ym(104280599,'init',{accurateTrackBounce:true,trackLinks:true});
