7c5c42a87e33358bbd91d5c37187e73a58b88d1f
nedicom <m6132@yandex.ru>
2025-09-27 19:31:43 +0300

    error

--- myproject/settings/base.py
+++ myproject/settings/base.py
@@ -147,7 +147,7 @@ STATICFILES_DIRS = [
 STATICFILES_STORAGE = "django.contrib.staticfiles.storage.ManifestStaticFilesStorage"
 
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
-STATIC_URL = "/static/"
+STATIC_URL = "myproject/static/"
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = "/media/"
