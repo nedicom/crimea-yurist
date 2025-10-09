3ceb9ec7878b7edbc25d12f32ad74c6bf2c33725
nedicom <m6132@yandex.ru>
2025-10-09 10:39:50 +0300

    subpage_types

--- home/models.py
+++ home/models.py
@@ -43,7 +43,7 @@ class HomePage(Page):
 
     # Настройки страницы
     parent_page_types = []  # Только в корне сайта
-    subpage_types = ['CityPage', 'home.HomePage', 'wagtailcore.Page'] # Можно создавать только страницы городов
+    subpage_types = ['CityPage', 'home.HomePage', 'wagtailcore.Page', 'PracticeGalleryPage']
     max_count = 1  # Только одна главная страница
 
     content_panels = Page.content_panels + [
